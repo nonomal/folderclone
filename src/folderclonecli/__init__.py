856f9f7b98f59d4fe47bd895f08d6d83330718a3
Spazzlo <19234742+Spazzlo@users.noreply.github.com>
2020-09-18 18:49:05 -0700

    automatically uninstall fc

--- src/folderclonecli/__init__.py
+++ src/folderclonecli/__init__.py
@@ -1,3 +1,6 @@
 def main():
     print("Due to changes to Google's APIs, Folderclone is dead.")
-    exit(1)
+    yn = input('would you like me to uninstall folderclone for you? [Y/n] ')
+    if len(yn) == 0 or yn.lower()[0] == 'y':
+        import subprocess, sys
+        subprocess.check_call([sys.executable, '-m', 'pip', 'uninstall', 'folderclone', '-y'])
